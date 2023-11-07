df1b21f19247812d5f3888a4f267ced7b08d81da
Nima <nimaseda@uw.edu>
2023-11-07 08:14:11 -0800

    Reproduce Figure 4 (qualitative illustration of results)

---
+++ models/fetch_models.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+wget -N https://www.rawdataspeaks.com/wp-content/uploads/projects/astro-machines/data/model_128d_e182_i1500000.pth.tar
+
--- train/fits_spec_datalayer.py
+++
@@ -1,9 +0,0 @@
-import torch.utils.data as data
-import torch
-import os
-import os.path
-import numpy as np
-
-
-class fits_spec_datalayer(data.Dataset):
-    pass
